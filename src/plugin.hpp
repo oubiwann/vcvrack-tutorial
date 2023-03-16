6981faf25cf548eae41f4807e9df6e178dddd33d
Duncan McGreggor <oubiwann@gmail.com>
2023-03-16 02:32:07 -0500

    Formatting.

--- src/plugin.hpp
+++ src/plugin.hpp
@@ -5,6 +5,5 @@ using namespace rack;
 
 // Declare the Plugin, defined in plugin.cpp
 extern Plugin *pluginInstance;
-
 // Declare each Model, defined in each module source file
 extern Model *modelTutorialMod;
